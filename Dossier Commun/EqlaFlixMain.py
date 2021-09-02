0057a966ed9dc4e62064c14148b3b0dd87c89b42
zackakyo <tcheuyassiisaac@yahoo.com>
2021-09-02 13:45:15 +0200

    dossier jeux

---
+++ Dossier Commun/EqlaFlixMain.py
@@ -0,0 +1,42 @@
+import sys
+import os
+
+os.system('cls')
+
+# cette fonction gére le menu principal de  EQLAFLIX
+def EQLAFLIX(): 
+    _menuSentence = "\n EQLA-FLIX \n 1- JEUX \n 2- FILMS \n 3- SERIES \n \"Q\"-	Pour quitter  \n Votre choix : "
+    _userChoice = input(_menuSentence).lower()
+    os.system('cls')
+    while _userChoice != "q":
+        try: 
+            _userChoice = int(_userChoice)
+            if _userChoice == 1 :
+                Jeux.DisplayFilmsMenu()
+            elif _userChoice == 2 : 
+                Films.DisplayFilmsMenu()
+            elif _userChoice == 3: 
+                Series.DisplayFilmsMenu()
+            else: 
+                _userChoice = input("\n Votre choix est incorrect; Veuillez entrer un choix valide. \n" + _menuSentence ).lower()
+                os.system('cls')
+            _userChoice = input(_menuSentence).lower()
+            os.system('cls')
+        except:
+            _userChoice = input( _menuSentence).lower()
+            os.system('cls')
+        print("\n")
+    else: 
+        sys.exit()
+
+
+EQLAFLIX()
+
+
+
+
+
+
+
+
+
